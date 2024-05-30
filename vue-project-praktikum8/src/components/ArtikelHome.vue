cc8ef9ec38ca48b69f084df1ef6c073c7c6ce662
thedims29 <dimasramadhan750@gmail.com>
2024-05-30 17:08:54 +0700

    Praktikum8

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <img src="" alt="">
+        <h1>Ini adalah komponen artikel home</h1>
+        <p>ini adalah isi artikel</p>
+        <p>ini adalah isi artikel</p>
+        <p>ini adalah isi artikel</p>
+    </div>
+</template>
---
+++ vue-project-praktikum8/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ArtikelHome from '../components/ArtikelHome.vue'
+
+</script>
+
+<template>
+  <main>
+    <ArtikelHome />
+  </main>
+</template>
